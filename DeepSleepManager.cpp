fcd7f507a3d0b0ab44a8d494686475f8ee9294eb
net234 <51489394+net234@users.noreply.github.com>
2021-02-26 17:29:35 +0100

    Version V1.1 en cours

--- DeepSleepManager.cpp
+++ DeepSleepManager.cpp
@@ -75,7 +75,9 @@ uint8_t DeepSleepManager::getRstReason(const int16_t buttonPin) {
     savedRTCmemory.remainingTime = 0;
     savedRTCmemory.actualTimestamp = 0;
     savedRTCmemory.powerOnTimestamp = 0;
-    savedRTCmemory.correction = 25000;  //ok 20mn
+    //savedRTCmemory.correction = 50000;  //ok  55000 5m
+    //savedRTCmemory.correction = 35972;  //35972 4H
+    savedRTCmemory.correction = 28929;  //28929 7H
     savedRTCmemory.startTimestamp = 0;
     savedRTCmemory.sleepTime = 0;
     savedRTCmemory.uncorrectedTime = 0;
@@ -122,14 +124,33 @@ void DeepSleepManager::permanentDeepSleep() {
   saveRTCmemory();
   ESP.deepSleep(0, RF_DEFAULT);
 }
+
+
+
+
+
+void     DeepSleepManager::deepSleepUntil(const uint8_t pHour, const uint8_t pMinute, const uint8_t pSecond,  uint16_t increment) {
+  tmElements_t tm;
+  time_t tmNow = now();
+  breakTime(tmNow, tm);
+  tm.Hour = pHour;
+  tm.Minute = pMinute;
+  tm.Second = pSecond;
+  time_t tmUntil = makeTime(tm);
+  if (tmUntil < tmNow)  {
+    tmUntil += 24 * 3600;
+  }
+  tmUntil -= tmNow;
+  startDeepSleep(tmUntil, increment);
+}
 //const int32_t adjust = 150000;//149300; +130
 //const int32_t adjust = 140000; +140
 //const int32_t adjust = 300000; //+.10
 //const int32_t adjust = 350000; // +.010
-//const int32_t adjust = 500000; //  -.01  corr=27382
+//const int32_t adjust = 550000; //  -.01  corr=27382
 //const int32_t adjust   = 0; //    corr = 2000000
-const int32_t adjust   = zz1350000; //    corr = 2000000
-zz
+const int32_t adjust   = 900000LL; //    corr = 2000000
+//const int32_t adjust   = 328929;
 void DeepSleepManager::startDeepSleep(const uint32_t sleepTimeSeconds, const uint16_t increment, const uint16_t offset ) { // start a deepSleepMode with   default increment 2 hours
   savedRTCmemory.actualTimestamp = now();
   savedRTCmemory.startTimestamp = savedRTCmemory.actualTimestamp;
@@ -166,7 +187,7 @@ void DeepSleepManager::startDeepSleep(const uint32_t sleepTimeSeconds, const uin
   Serial.print(float(microsDelay));
   Serial.println(".");
   //if (nextIncrement > 0) ESP.deepSleep(microsDelay - micros() - 149300 , (savedRTCmemory.remainingTime > 0 ) ? RF_DISABLED : RF_DEFAULT);  //2094
-  if (nextIncrement > 0) ESP.deepSleep(microsDelay , (savedRTCmemory.remainingTime > 0 ) ? RF_DISABLED : RF_DEFAULT);  //2094
+  if (nextIncrement > 0) ESP.deepSleep(microsDelay - adjust, (savedRTCmemory.remainingTime > 0 ) ? RF_DISABLED : RF_DEFAULT);  //2094
   // if (nextIncrement > 0) ESP.deepSleep(nextIncrement * 1.004 * 1E6 - micros() - 149300 , (savedRTCmemory.remainingTime > 0 ) ? RF_DISABLED : RF_DEFAULT);  //2094
 
 
@@ -261,16 +282,17 @@ void     DeepSleepManager::setActualTimestamp(time_t timestamp) {   // save time
     Serial.println(delta);
   }
   delta--;
-  if ( savedRTCmemory.uncorrectedTime > 0 ) {
+  if ( savedRTCmemory.uncorrectedTime > 0 && timeStatus() == timeSet) {
     D_println(savedRTCmemory.uncorrectedTime);
-    if (delta != 0) {
+    if (delta != 0 && savedRTCmemory.uncorrectedTime > 100  ) {  // no correction on less than 100 sec or more than 100ms correction
       int32_t corr = 1000000L * delta;
-      D_println(corr);
+      //D_println(corr);
       corr = 1000000L * delta / savedRTCmemory.uncorrectedTime;
       D_println(corr);
-      savedRTCmemory.correction -= corr;
-    } else {
-      Serial.println("No correction");
+      if (abs(corr) < 100000) {
+        Serial.println("correction done ");
+        savedRTCmemory.correction -= corr;
+      }
     }
     D_println(savedRTCmemory.correction);
     savedRTCmemory.uncorrectedTime = 0;
