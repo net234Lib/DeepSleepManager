8ccd1b4b8ef6992cc8626940622f021c7dafc83f
net234 <51489394+net234@users.noreply.github.com>
2021-02-26 22:44:29 +0100

    pb sendmail

--- exemple/mesureAndSendMailLater/mesureAndSendMailLater.ino
+++ exemple/mesureAndSendMailLater/mesureAndSendMailLater.ino
@@ -7,53 +7,53 @@
    Then send a mail with the result
    B02
      add AUTH for mail
-     
+
 
    // This exemple need :
   // A connection between D0 (GPIO16) and RST with a 1K resistor or a diode see documentation
   // A DHT11 (the temp/humidity sensor of your Arduino Kit) connected to D6 (GPIO12)
   // Eventualy a user button to abort a very long deep sleep (here BP0) connected to D7 (GPIO13)
 
- 
+
   mesures : nodeMcu dev board CP2120 ESP8266-E12 3v3
   Wifi connected modem  =  24ma / 28ma / 77ma / 190ma
-  Deep sleep            = 9,3ma 
+  Deep sleep            = 9,3ma
 
   mesures : nodeMcu dev board CP2120 ESP8266-E12 3v3 without AMS117
   Wifi connected modem  =  25ma / 29ma / 80ma / 190ma
-  Deep sleep            = 7,8ma 
-  
-  mesures : nodeMcu dev board CP2120 ESP8266-E12 5V usb or 5V Vin 
+  Deep sleep            = 7,8ma
+
+  mesures : nodeMcu dev board CP2120 ESP8266-E12 5V usb or 5V Vin
   Wifi connected modem  =  25ma / 29ma / 80ma / 190ma
-  Deep sleep            = 10,2ma 
+  Deep sleep            = 10,2ma
 
   mesures : nodeMcu dev board CH340 ESP8266-E12 3v3
   Wifi connected modem  =  14ma / 18ma / 70ma / 180ma
-  Deep sleep            = 1,8ma 
+  Deep sleep            = 1,8ma
 
   mesures : nodeMcu dev board CH340 ESP8266-E12 3v3 without AMS117
   Wifi connected modem  =  14ma / 18ma / 70ma / 180ma
-  Deep sleep            = 0,15ma 
+  Deep sleep            = 0,15ma
 
   mesures : nodeMcu dev board V3 CP2120 ESP8266-E12 5V (4,2V)
-  Wifi AP              =  70ma 
-  Deep sleep            = 1,54ma 
+  Wifi AP              =  70ma
+  Deep sleep            = 1,54ma
 
-  
+
   mesures : nodeMcu dev board V3 CP2120 ESP8266-E12F 3v3 (3,6V)
-  Wifi AP               =  70ma 
-  Deep sleep            = 1,7ma 
+  Wifi AP               =  70ma
+  Deep sleep            = 1,7ma
 
   mesures : nodeMcu dev board V3 CP2120 ESP8266-E12F 3v3 without AMS117
-  Wifi AP               =  70 ma 
-  Deep sleep            = 0,03ma 
- 
+  Wifi AP               =  70 ma
+  Deep sleep            = 0,03ma
+
   mesures : ESP8266-E12S 3v3 Alone
   Wifi AP               =  70 ma
-  Deep sleep            = 0,03ma 
+  Deep sleep            = 0,03ma
 
 
-  
+
   TODO: log if mail sended
 
 */
@@ -132,6 +132,10 @@ void setup() {
   if ( rstReason == REASON_DEEP_SLEEP_AWAKE  ) {
     // here we start serial to show that we are awake
     D_println(MyDeepSleepManager.getBootCounter());
+    Serial.print("MyDeepSleepManager.getPowerOnTimestamp = ");
+    Serial.println(niceDisplayTime(MyDeepSleepManager.getPowerOnTimestamp()));
+    Serial.print("MyDeepSleepManager.getActualTimestamp  = ");
+    Serial.println(niceDisplayTime(MyDeepSleepManager.getActualTimestamp()));
     D_println(MyDeepSleepManager.getRemainingTime());
 
 
@@ -141,7 +145,7 @@ void setup() {
 
     // Get local time
     time_t localTime = now();
-    D_println(Ctime(localTime));
+    D_println(niceDisplayTime(localTime));
 
     // Init FS system and save Value and localTime on  local File:data.csv
     MyFS.begin();
@@ -187,9 +191,9 @@ void setup() {
   Serial.println(MyDeepSleepManager.getRemainingTime());
   Serial.print("MyDeepSleepManager.getBootTimeStamp = ");
 
-  Serial.println(Ctime(MyDeepSleepManager.getBootTimestamp()));
+  Serial.println(niceDisplayTime(MyDeepSleepManager.getBootTimestamp()));
   Serial.print("MyDeepSleepManager.getPowerOnTimeStamp = ");
-  Serial.println(Ctime(MyDeepSleepManager.getPowerOnTimestamp()));
+  Serial.println(niceDisplayTime(MyDeepSleepManager.getPowerOnTimestamp()));
 
 
 
@@ -275,7 +279,8 @@ void loop() {
     // on connection to WiFI we try to setup clock and send mail
     do {
       if (WiFiStatus != WL_CONNECTED)  break;
-
+      setSyncProvider(getWebTime);
+      setSyncInterval(60 * 10);
       connectedTimeStamp = now();
 
       Vcc = getVcc();
@@ -284,15 +289,17 @@ void loop() {
         logDataCSV(F("No Internet connection"));
         break;
       }
+
+
       // dsisplay RTC status
       Vcc = getVcc();
       logDataCSV( "WWW Connected Vcc=" + String(Vcc)  );
       Serial.print("now() = ");
-      Serial.println(Ctime(now()));
+      Serial.println(niceDisplayTime(now()));
       Serial.print("bootTime = ");
-      Serial.println( Ctime(MyDeepSleepManager.getBootTimestamp()) );
+      Serial.println( niceDisplayTime(MyDeepSleepManager.getBootTimestamp()) );
       Serial.print("powerTime = ");
-      Serial.println( Ctime(MyDeepSleepManager.getPowerOnTimestamp()) );
+      Serial.println( niceDisplayTime(MyDeepSleepManager.getPowerOnTimestamp()) );
 
 
       // do we restart from a end of deep sleep ?
@@ -318,11 +325,11 @@ void loop() {
         logDataCSV(F("tried an email"));
       }
       //logDataCSV(F("-- start DeepSleep again for 1 Min with a 10 sec incremental offset 2"));
-      logDataCSV(F("-- start DeepSleep again for 3 Hour with a 10 Minutes incremental"));
+      logDataCSV(F("-- start DeepSleep with 10 minutes mesures mail at 22H30"));
       logDataCSV("Millsec was " + String(millis()));
       Serial.println(F("<-- GO"));
       //MyDeepSleepManager.startDeepSleep( 60, 10, 2 );
-      MyDeepSleepManager.startDeepSleep( 3 * 60 * 60, 10 * 60, 10 );
+      MyDeepSleepManager.deepSleepUntil( 22,0,0, 10 * 60 );
     } while (false);
 
   }
@@ -391,7 +398,7 @@ void loop() {
     }
     if (aChar == 'N') {
       Serial.print(F(" now() = "));
-      Serial.println(Ctime(now()));
+      Serial.println(niceDisplayTime(now()));
       //      anow = time(nullptr);
       //      Serial.print(F(" time() = "));
       //      Serial.println(Ctime(&anow));
@@ -450,19 +457,15 @@ bool connectedToInternet() {
   //      www.msftncsi.com  timestamp about 1 second
   //  https://success.tanaza.com/s/article/How-Automatic-Detection-of-Captive-Portal-works
   // in fact any know hhtp web server redirect on https  so use your FAI web url
-#define CAPTIVE HTTP_SERVER
 
-  Serial.println(F("connect to " CAPTIVE " to get time and check connectivity to www"));
+
+  Serial.println(F("connect to " HTTP_SERVER " to get time and check connectivity to www"));
 
   WiFiClient client;
   HTTPClient http;  //Declare an object of class HTTPClient
 
-  http.begin(client, "http://" CAPTIVE); //Specify request destination
+  http.begin(client, "http://" HTTP_SERVER); //Specify request destination
   // we need date to setup clock
-  const char * headerKeys[] = {"date"} ;
-  const size_t numberOfHeaders = 1;
-  http.collectHeaders(headerKeys, numberOfHeaders);
-
   int httpCode = http.GET();                                  //Send the request
   if (httpCode < 0) {
     Serial.print(F("cant get an answer http.GET()="));
@@ -471,44 +474,6 @@ bool connectedToInternet() {
     return (false);
   }
 
-  // we got an answer
-  String headerDate = http.header(headerKeys[0]);
-  // try to setup Clock
-  D_println(headerDate);
-  if (headerDate.endsWith(" GMT") && headerDate.length() == 29) {
-    tmElements_t dateStruct;
-    // we got a date
-    dateStruct.Second  = headerDate.substring(23, 25).toInt();
-    dateStruct.Minute  = headerDate.substring(20, 22).toInt();
-    dateStruct.Hour = headerDate.substring(17, 19).toInt();
-    dateStruct.Year = headerDate.substring(12, 16).toInt() - 1970;
-    const String monthName = F("JanFebMarAprJunJulAugSepOctNovDec");
-    dateStruct.Month = monthName.indexOf(headerDate.substring(8, 11)) / 3 + 1;
-    dateStruct.Day = headerDate.substring(5, 7).toInt();
-
-    //          Serial.print(dateStruct.tm_hour); Serial.print(":");
-    //          Serial.print(dateStruct.tm_min); Serial.print(":");
-    //          Serial.print(dateStruct.tm_sec); Serial.print(" ");
-    //          Serial.print(dateStruct.tm_mday); Serial.print("/");
-    //          Serial.print(dateStruct.tm_mon); Serial.print("/");
-    //          Serial.print(dateStruct.tm_year); Serial.println(" ");
-    time_t serverTS = makeTime(dateStruct) + 3600;
-
-    Serial.print("Server time = ");
-    Serial.println(Ctime(serverTS));
-    Serial.print("Local  time = ");
-    time_t nowTS = now();
-    Serial.println(Ctime(nowTS));
-    int delta = serverTS - nowTS;
-    logDataCSV("delta time : " + String(delta));
-    if (abs(delta) > 1) {
-      D_println(timeStatus());
-      logDataCSV("Settime(" + Ctime(serverTS) + ")");
-      setTime(serverTS);
-      MyDeepSleepManager.setActualTimestamp();
-      D_println(timeStatus());
-    }
-  }
 
   //Date: Mon, 25 Jan 2021 21:18:52 GMT
   String payload = http.getString();   //Get the request response payload
@@ -547,9 +512,14 @@ bool sendDataCsvTo(const String sendto)  {
   WiFiClient tcp;  //Declare an object of Wificlass Client to make a TCP connection
   String aLine;    // to get answer of SMTP
   // Try to find a valid smtp
+   if ( !tcp.connect(SMTP_SERVER, 25) ) {
+    Serial.println("unable to connect with " SMTP_SERVER ":25" );
+    return false;
+  }
+
   bool valideSmtp = false;
   do {
-
+     
     Serial.println("connected with " SMTP_SERVER ":25" );
     aLine = tcp.readStringUntil('\n');
     if (!aLine.startsWith("220 "))  break;  //  not good answer
@@ -588,7 +558,7 @@ bool sendDataCsvTo(const String sendto)  {
       while (f.available()) {
         String aTime = f.readStringUntil('\t');
         String aLine = f.readStringUntil('\n');
-        tcp.print(Ctime(aTime.toInt()));
+        tcp.print(niceDisplayTime(aTime.toInt()));
         tcp.print('\t');
         tcp.print(aTime);
         tcp.print('\t');
@@ -614,12 +584,12 @@ bool sendDataCsvTo(const String sendto)  {
         tcp.print(F("AUTH LOGIN \r\n"));
         aLine = tcp.readStringUntil('\n');
         if (aLine.toInt() != 334 )  break;  //  not good answer
-        
+
         //Serial.println(F("SEND LOGIN"));
         tcp.print(F(SMTP_LOGIN "\r\n"));
         aLine = tcp.readStringUntil('\n');
         if (aLine.toInt() != 334 )  break;  //  not good answer
-        
+
         //Serial.println(F("SEND PASS"));
         tcp.print(F(SMTP_PASS "\r\n"));
         aLine = tcp.readStringUntil('\n');
@@ -656,7 +626,7 @@ bool sendDataCsvTo(const String sendto)  {
         while (f.available()) {
           String aTime = f.readStringUntil('\t');
           String aLine = f.readStringUntil('\n');
-          tcp.print(Ctime(aTime.toInt()));
+          tcp.print(niceDisplayTime(aTime.toInt()));
           tcp.print('\t');
           tcp.print(aTime);
           tcp.print('\t');
@@ -695,7 +665,7 @@ void printDataCSV() {
     while (f.available()) {
       time_t aTime = f.readStringUntil('\t').toInt();
       String aLine = f.readStringUntil('\n');
-      Serial.print(Ctime(aTime));
+      Serial.print(niceDisplayTime(aTime));
       Serial.print('\t');
       Serial.println(aLine);
     }
@@ -718,7 +688,7 @@ void logDataCSV(const String atext) {
   Serial.print(F("Log: "));
   Serial.print(atext);
   Serial.print(F(" at "));
-  Serial.println(Ctime(aNow));
+  Serial.println(niceDisplayTime(aNow));
 
 }
 
@@ -727,6 +697,8 @@ void logDataCSV(const String atext) {
 
 
 
+
+// display time a nice way
 String str2digits(const uint8_t digits) {
   String txt;
   if (digits < 10)  txt = '0';
@@ -734,19 +706,27 @@ String str2digits(const uint8_t digits) {
   return txt;
 }
 
-//String Ctime(time_t time) {
-//  return String(ctime(&time)).substring(0, 24);
-//};
 
 
-String Ctime(time_t time) {
+String niceDisplayTime(time_t time) {
+
+  String txt;
+  // we supose that time < NOT_A_DATE_YEAR is not a date
+  if ( year(time) < NOT_A_DATE_YEAR ) {
+    txt = "          ";
+    txt += time / (24 * 3600);
+    txt += ' ';
+    txt = txt.substring(txt.length() - 10);
+  } else {
+
+    txt = str2digits(day(time));
+    txt += '/';
+    txt += str2digits(month(time));
+    txt += '/';
+    txt += year(time);
+  }
+
   static String date;
-  String txt = "";
-  txt += str2digits(day(time));
-  txt += '/';
-  txt += str2digits(month(time));
-  txt += '/';
-  txt += year(time);
   if (txt == date) {
     txt = "";
   } else {
